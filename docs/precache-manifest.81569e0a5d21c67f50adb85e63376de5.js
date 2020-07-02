cf50798b0b07360729a7c454514ed2c208bdb3cd
Ronnie Renso Velasquez Lupa <ronnieitor@gmail.com>
2020-07-01 21:35:53 -0400

    Docs agregado

---
+++ docs/precache-manifest.81569e0a5d21c67f50adb85e63376de5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d82b07a453bb1385a3ede26258f4f4c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fed22045dc5bee94938",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "24a6f2f9df03bebb151e",
+    "url": "/static/css/main.8a440dcb.chunk.css"
+  },
+  {
+    "revision": "4fed22045dc5bee94938",
+    "url": "/static/js/2.7f4a3fdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7f4a3fdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24a6f2f9df03bebb151e",
+    "url": "/static/js/main.bc8eb1b4.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
